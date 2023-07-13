e106bc78e6bafdd67df940f8067a1002da5d10d4
Leo <playingwood@gmail.com>
2023-07-13 17:37:28 +0800

    add article function and save to DB

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,28 @@
 class ArticlesController < ApplicationController
     def index
+    end
+  
+    def new
+        # @實體變數
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params) 
+
+        if @article.save
+            redirect_to "/articles", notice:"文章新增成功囉 ! "
+        else
+            render :new
+            # redirect_to "/articles/new", alert:"文章新增失敗囉 QQ ! "
         end
+            
+
+        # flash[:notice] = "文章新增成功囉 ! "
+        # redirect_to "/articles", notice:"文章新增成功囉 ! "
+        # render html: params
+    end
+    def article_params
+        params.require(:article).permit(:title, :context)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "articles#index"
+  
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new'
+
+  post '/articles', to: 'articles#create'
+
+  get '/about', to: 'pages#about'
 end
